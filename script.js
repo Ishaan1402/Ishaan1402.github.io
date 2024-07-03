6aaf21c10d91ff224efaebc419fb242e8f4ba71a
Ishaan Patel <ishbhagat02@gmail.com>
2024-07-03 10:48:25 -0400

    Update script.js
    commit from test repo

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky")
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
         }
-        else{
-            $('.navbar').removeClass("sticky")
-        }
-    })
-    $('.menu-btn').click(function(){
+    });
+
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    $('.navbar .menu li a').click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+        $('.navbar .menu').removeClass("active");
+        $('.menu-btn i').removeClass("active");
     });
 });
